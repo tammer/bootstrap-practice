0a0fbbbf4d2bfdbcf0edc2d471e86a5173485e98
Tammer <tammer@tammer.com>
2022-06-05 00:14:19 -0400

    not telling

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -39,35 +39,41 @@ const Skills = () => {
               <dl className="row">
                 <dt className="col-sm-3">Novice</dt>
                 <dd className="col-sm-9">
-                  Exaples of novice level include having trialed this technology
-                  in sandbox or "toy app" environments or having been exposed to
-                  a the technology in an educational setting, but not used it
-                  professionally yet.
+                  You have trialed the technology in sandbox and "toy app"
+                  environments or have been exposed to it in an educational
+                  setting. You have not used the technology in a professional
+                  setting.
                 </dd>
                 <dt className="col-sm-3">Capable</dt>
                 <dd className="col-sm-9">
-                  You have good understanding of the technology and can be
-                  productive while continuing to round out your skills in the
-                  tech. you might need to reference docs and sampel code often,
-                  but are able to leverage that into productivity.
+                  You have an understanding of the technology and have used it
+                  in a professional setting. When working with the technology
+                  you probably make frequent use of internet resources to
+                  continue to learn it and to overcome obsticles, but you are
+                  nonetheless able to use the technology productively.
                 </dd>
                 <dt className="col-sm-3">Proficient</dt>
                 <dd className="col-sm-9">
-                  You know this technology well. You are productive on a day to
-                  day basis, occationally needing to consult documentation or
-                  Google.
+                  You know this technology well and are approaching expert level
+                  on many aspects of it. You are productive with this
+                  technology, occationally consulting documentation and internet
+                  resources. There are certain advanced features and/or
+                  techniques that you do not yet know thoroughly.
                 </dd>
                 <dt className="col-sm-3">Expert</dt>
                 <dd className="col-sm-9">
-                  Expert implies an mastery of the technology. You understand it
-                  completely. You are almost certainly the most capable
-                  pracitioner in your workplace. And as good as anyone you know
-                  at it.
+                  You have mastered the technology. There is essentially nothing
+                  you don't know about it. You are able to leverage the
+                  technology for any feasible purpose effeciently, rarely
+                  needing to consult documentation or internet resources. You
+                  are probably the most knowledge person on this technology in
+                  your organization.
                 </dd>
                 <dt className="col-sm-3">Authority</dt>
                 <dd className="col-sm-9">
-                  You are part of the group of people who create, develop,
-                  maintain and evolve this technology.
+                  You are not only an expert; you are part of the group of
+                  people who create, develop, maintain and evolve this
+                  technology.
                 </dd>
               </dl>
             </div>
