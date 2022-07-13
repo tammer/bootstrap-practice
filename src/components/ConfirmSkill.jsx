f937e76a11fd3c65fdedb4f3edea880cbf4b24dc
Tammer <tammer@tammer.com>
2022-07-14 09:24:48 +1000

    all three scenarios now covered

--- src/components/ConfirmSkill.jsx
+++ src/components/ConfirmSkill.jsx
@@ -1,9 +1,48 @@
 import { Modal, Button } from "react-bootstrap";
 import { acceptAnchor, declineAnchor } from "../helpers/helpers";
 
-import { FrozenLevelSlider } from "./LevelSlider";
+import LevelSlider, { FrozenLevelSlider } from "./LevelSlider";
 
 const ConfirmSkill = ({ spec, show = true, onHide, onAccept, onDecline }) => {
+  const showMessage2 = () => (
+    <>
+      {spec["passer_display_name"] + " "}
+      is inviting you to levelset on
+      {" " + spec["skill"]}. However, your skill levels are quite different:
+      <br />
+      <br />
+      <p>{spec["passer_first_name"]}'s Level:</p>
+      <LevelSlider frozen value={spec["level"]} />
+      <br />
+      <p>Your Level:</p>
+      <LevelSlider frozen value={spec["my_level"]} />
+      <br />
+      <br />
+      <p>If this difference is because:</p>
+      <ul>
+        <li>
+          you have
+          {spec["level"] < spec["my_level"] ? " overstated " : " understated "}
+          your {spec["skill"] + " "} skill level, then close this dialog, adjust
+          your {spec["skill"] + " "} skill level then return.
+        </li>
+        <li>
+          {spec["passer_first_name"] + " "} has
+          {spec["level"] > spec["my_level"] ? " overstated " : " understated "}
+          their {spec["skill"] + " "} skill level, then you should decline this
+          invite. ({spec["passer_first_name"] + " "} can adjust their self
+          assessment to something more accurate. Then{" "}
+          {spec["passer_first_name"] + " "} or you can re-issue an invite.)
+        </li>
+        <li>
+          you are in fact at different skill levels, then simply decline the
+          invite. (Inaccurate endorsements degrade your credibility. Learn
+          more>>)
+        </li>
+      </ul>
+    </>
+  );
+
   const showMessage = () => (
     <>
       {spec["passer_display_name"] + " "}
@@ -66,8 +105,7 @@ const ConfirmSkill = ({ spec, show = true, onHide, onAccept, onDecline }) => {
       <Modal.Header closeButton>
         <Modal.Title>{spec["skill"]}</Modal.Title>
       </Modal.Header>
-      {/* {spec["my_level"] ? existingSkillMessage : newSkillMessage} */}
-      {showMessage()}
+      {spec["confirmable"] ? showMessage() : showMessage2()}
       <Button
         onClick={() => declineAnchor(spec["id"]).then((res) => onDecline(res))}
         variant="secondary"
@@ -75,6 +113,7 @@ const ConfirmSkill = ({ spec, show = true, onHide, onAccept, onDecline }) => {
         Decline
       </Button>
       <Button
+        disabled={!spec["confirmable"]}
         onClick={() => acceptAnchor(spec["id"]).then((res) => onAccept(res))}
         variant="primary"
       >
