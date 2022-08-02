80372352a0637e12b3ec3f6bce38f85a75691a69
Tammer <tammer@tammer.com>
2022-08-03 06:51:28 +1000

    react-switch

---
+++ src/components/bpswitch.jsx
@@ -0,0 +1,24 @@
+import Switch from "react-switch";
+import { useState } from "react";
+
+const BPSwitch = ({ checked, handleChange }) => {
+  return (
+    <>
+      <span>RELEVANT?</span>
+      <Switch
+        height={18}
+        width={40}
+        onChange={handleChange}
+        checked={checked}
+        className="react-switch"
+        // handleDiameter={100}
+        uncheckedIcon={<div className="bpswitch-off">NO</div>}
+        checkedIcon={<div className="bpswitch-on">YES</div>}
+        onColor="#61FACC"
+        activeBoxShadow="0px 0px 0px 0px rgba(0, 0, 0, 0.2)"
+      />
+    </>
+  );
+};
+
+export default BPSwitch;
