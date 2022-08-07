ad3b19257124c9304b33949109b0eca31f2f5a4b
Tammer <tammer@tammer.com>
2022-08-08 05:43:37 +1000

    tech stack coming along

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import LevelSlider from "../components/LevelSlider";
+import SkillSelector from "../components/SkillSelector";
 
 const TechStack = ({ attributes, handleChange }) => {
+  const [selectorValue, setSelectorVlaue] = useState(null);
   function handleLevelChange(skillName, newLevel) {
     let rv = [...attributes];
     rv.find((e) => e["name"] == skillName)["level"] = newLevel;
     handleChange(rv);
   }
+
+  function newSkill(value) {
+    if (attributes.find((e) => e["name"] == value["name"])) {
+      return;
+    }
+    value["level"] = 50;
+    setSelectorVlaue(null);
+    handleChange([...attributes, value]);
+  }
+
+  //   function buttonRow() {
+  //     return (
+  //       <div className="level-slider-outer">
+  //         <div style={{ gridColumn: "1 /3" }}>{/* <SkillSelector /> */}</div>
+
+  //         {/* <div className="horse-holder">
+  //           <div className="horse-new">+ New</div>
+  //         </div> */}
+  //       </div>
+  //     );
+  //   }
 
   function row(name, level) {
     return (
@@ -34,19 +58,30 @@ const TechStack = ({ attributes, handleChange }) => {
   }
 
   return (
-    <div className="level-slider-box">
-      <div className="level-slider-outer-header">
-        <div />
-        <div className="level-slider-header">
-          <div>novice</div>
-          <div>capable</div>
-          <div>proficient</div>
-          <div>expert</div>
-          <div>authority</div>
+    <>
+      <div className="level-slider-box">
+        <div className="level-slider-outer-header">
+          <div />
+          <div className="level-slider-header">
+            <div>novice</div>
+            <div>capable</div>
+            <div>proficient</div>
+            <div>expert</div>
+            <div>authority</div>
+          </div>
         </div>
+        {attributes.map((att) => row(att["name"], att["level"]))}
+        {/* {buttonRow()} */}
       </div>
-      {attributes.map((att) => row(att["name"], att["level"]))}
-    </div>
+      <br />
+      <div style={{ width: "300px" }}>
+        <SkillSelector
+          isMulti={false}
+          handleChange={newSkill}
+          value={selectorValue}
+        />
+      </div>
+    </>
   );
 };
 
