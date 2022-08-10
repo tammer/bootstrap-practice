c2dc45a6e1794e57d7eb84d4fb16dc17d3062db1
Tammer <tammer@tammer.com>
2022-08-10 11:55:59 +1000

    im smarter now than when i first wrote this.

--- src/components/Privilaged.jsx
+++ src/components/Privilaged.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const Privilaged = (props) => {
-  const [authorized, setAuthorized] = useState(
-    localStorage.getItem("token") ? true : false
-  );
+  const authorized = localStorage.getItem("token") ? true : false;
 
   function redirect() {
     localStorage.setItem("next", window.location.pathname);
