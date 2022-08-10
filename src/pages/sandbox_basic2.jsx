64c10e8e7bece37fb213d91ee00b9f389acaae6a
Tammer <tammer@tammer.com>
2022-08-10 19:55:34 +1000

    whatever

--- src/pages/sandbox_basic2.jsx
+++ src/pages/sandbox_basic2.jsx
@@ -1,19 +1,4 @@
-const Modal = ({ title, body, visible = false }) => {
-  return (
-    <>
-      {visible ? (
-        <div className="bp-modal">
-          <div>
-            {<h1>{title}</h1>}
-            {body}
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
-    </>
-  );
-};
+import { Modal } from "../components/Modal";
 
 const Sandbox = () => {
   return (
