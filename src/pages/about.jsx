30420b00047c70c22316bce1571528a5b52cf598
Tammer <tammer@tammer.com>
2022-11-01 20:48:34 -0400

    cleanup

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,11 +7,7 @@ const About = () => {
         <div className="right-panel">
           <div className="left-subpanel">
             <h1>About</h1>
-            <p>
-              The pseudocode shows the filtering that is applied to every job
-              posting submitted to this platform. It follows directly from your
-              spec.
-            </p>
+            <p>At some point I will put something here.</p>
           </div>
           <div className="right-subpanel">
             <div className="attribute-section">
@@ -19,14 +15,7 @@ const About = () => {
                 <div>
                   <ReactMarkdown>{`# Hello!
 
-There are basically two tasks:
 
-1) trial the service. (I'll drop you on the landing page with minimal info to see what happens.)
-2) give me feedback (via a google form)
-
-
-
-Please use a computer ( since there is no mobile UX yet.)
 
 Thank you!!!!
         
